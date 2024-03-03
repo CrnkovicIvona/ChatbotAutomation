1f10debd318d797ce3b0c2a926a9f808e3d97b99
CrnkovicIvona <crnkovicivona@gmail.com>
2024-03-03 15:54:41 +0100

    First commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,22 +1,23 @@
 package org.example;
-import dev.failsafe.internal.util.Durations;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
-
 import org.openqa.selenium.devtools.v119.network.Network;
 import org.openqa.selenium.devtools.v119.network.model.WebSocketFrame;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import java.time.*;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.By;
 import org.openqa.selenium.devtools.DevTools;
-
+import java.time.Duration;
 import java.util.Base64;
 import java.util.Optional;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.io.IOException;
+import java.io.File;
 
 public class Main {
-    private static void handleWebSocketFrame(Object webSocketFrameObject) {
+    private static String lastReceivedMessage = "";
+
+    private static void handleWebSocketFrame(WebDriver driver, Object webSocketFrameObject) {
         if (webSocketFrameObject instanceof WebSocketFrame) {
             WebSocketFrame webSocketFrame = (WebSocketFrame) webSocketFrameObject;
 
@@ -25,7 +26,10 @@ public class Main {
 
             // Process the WebSocket message
             System.out.println("Received WebSocket message: " + decodedPayload);
-            // Add your code to display or process the received WebSocket message here
+            lastReceivedMessage = decodedPayload;
+
+            // Print out the updated lastReceivedMessage
+            System.out.println("Updated lastReceivedMessage: " + lastReceivedMessage);
         }
     }
 
@@ -39,13 +43,11 @@ public class Main {
 
         // Set up a listener to handle WebSocket frames
         devTools.addListener(Network.dataReceived(), webSocketFrameReceived ->
-                handleWebSocketFrame(webSocketFrameReceived));
-
+                handleWebSocketFrame(driver, webSocketFrameReceived));
 
         try {
             // Specify the URL you want to visit
             String url = "https://www.rba.hr/korisne-informacije/rba-chatbot";
-            // Set up a listener to handle WebSocket frames
 
             driver.get(url);
             WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
@@ -57,16 +59,18 @@ public class Main {
             agree.click();
             WebElement iframe = driver.findElement(By.id("ib-iframe-messaging"));
             driver.switchTo().frame(iframe);
-           WebElement messageBox = wait.until((ExpectedConditions.visibilityOfElementLocated(By.className("ib-widget-message-input"))));
-           messageBox.click();
-           messageBox.sendKeys("testing 123");
+
+            String promptText = new String(Files.readAllBytes(Paths.get("C:\\Users\\ivona\\qa-rba-master\\src\\main\\java\\org\\example\\prompts.txt")));
+
+            WebElement messageBox = wait.until((ExpectedConditions.visibilityOfElementLocated(By.className("ib-widget-message-input"))));
+            // Use promptText instead of hardcoded string
+            messageBox.click();
+            messageBox.sendKeys(promptText);
 
             WebElement sendMessage =  driver.findElement(By.cssSelector("button[data-testid='input-submit']"));
             sendMessage.click();
 
             String cssSelector = ".ib-widget-agent-messages-section";
-
-            int prevElementsCount = 0, currentCount = 0;
 
             Thread.sleep(10000);
             // Wait for the elements to be present on the page
@@ -78,9 +82,36 @@ public class Main {
             for(WebElement elem: elements){
                 System.out.println(elem.getText());
             }
-        }catch (InterruptedException e) {
+
+            // Compare the last received message with the expected result
+            String expectedResponse = new String(Files.readAllBytes(Paths.get("C:\\Users\\ivona\\qa-rba-master\\src\\main\\java\\org\\example\\expected_results.txt")));
+
+// Print out the messages that are being compared
+            System.out.println("Last received message: " + lastReceivedMessage);
+            System.out.println("Expected response: " + expectedResponse);
+
+            if(!lastReceivedMessage.equals(expectedResponse)) {
+                System.out.println("The received message does not match the expected result.");
+                // Take a screenshot
+                File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+                File screenshotLocation = new File("C:\\Users\\ivona\\qa-rba-master\\screenshots\\screenshot.png");
+                if(screenshotLocation.exists()) {
+                    // If the file already exists, append a timestamp to the filename
+                    screenshotLocation = new File("C:\\Users\\ivona\\qa-rba-master\\screenshots\\screenshot" + System.currentTimeMillis() + ".png");
+                }
+                Files.copy(screenshot.toPath(), screenshotLocation.toPath());
+            }
+
+        } catch (InterruptedException | IOException e) {
             // Handle the exception
             e.printStackTrace();
+            // Take a screenshot
+            File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+            try {
+                Files.copy(screenshot.toPath(), Paths.get("C:\\Users\\ivona\\qa-rba-master\\screenshots\\screenshot.png"));
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
         }  finally {
             // Close the WebDriver
             driver.quit();
